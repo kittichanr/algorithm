8020e88ed86cc18f63f49d61211e9c3adc8fedcd
kittichanr <petchkubbb@gmail.com>
2024-04-13 14:32:20 +0700

    refactor name folder

--- 04_quick_sort/main.go
+++ 04_quick_sort/main.go
@@ -34,8 +34,32 @@ func quickSort(list []int) []int {
 	return concat
 }
 
+func quickSortPivotFirst(list []int) []int {
+	if len(list) < 2 {
+		return list
+	}
+
+	var less []int
+	var greater []int
+
+	pivot := list[0]
+
+	for _, v := range list[1:] {
+		if v <= pivot {
+			less = append(less, v)
+		} else {
+			greater = append(greater, v)
+		}
+	}
+
+	concat := slices.Concat(quickSort(less), []int{pivot}, quickSort(greater))
+
+	return concat
+}
+
 func main() {
 	list := []int{10, 5, 2, 3, 1}
 
 	fmt.Println(quickSort(list))
+	fmt.Println(quickSortPivotFirst(list))
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module github.com/kittichanr/algorithm
 
-go 1.21.6
+go 1.22.0
