7b52310cb7cb432556d3ac799e4f46377bceae01
kittichanr <petchkubbb@gmail.com>
2024-04-15 14:35:59 +0700

    hash table

---
+++ 05_hash_table/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+const approve = "let them vote!"
+const reject = "kick them out!"
+
+var voted = map[string]bool{}
+
+func check(name string) string {
+	if _, ok := voted[name]; ok {
+		return reject
+	}
+	voted[name] = true
+	return approve
+}
+
+func main() {
+	fmt.Println(check("john"))
+	fmt.Println(check("tom"))
+	fmt.Println(check("tom"))
+}
